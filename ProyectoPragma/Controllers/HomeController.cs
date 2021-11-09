cfea97034219910747378b4321a2d4da24e1082e
martinrosales2000 <martin.rosales@catolica.edu.sv>
2021-11-09 10:10:17 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoPragma/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoPragma.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+        public ActionResult Inventario()
+        {
+            return View();
+        }
+        public ActionResult Ingresar_Ventas()
+        {
+            return View();
+        }
+        public ActionResult Historial_Ventas()
+        {
+            return View();
+        }
+        public ActionResult Lote()
+        {
+            return View();
+        }
+    }
+}
